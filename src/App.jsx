f1c6ec5b83472094231bc0122e458a82aba1d6f4
Tigran5811 <tigran.bayramyan3@gmail.com>
2023-01-14 20:25:54 +0400

    buten component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import className from 'classnames/bind';
 import { Button } from './components/button/Button';
+import styles from './style.scss';
+
+const cx = className.bind(styles);
 
 export class App extends Component {
-  constructor() {
-    super();
-    state = {
-      type: 'button',
-    };
-  }
+  state = {
+    isOpenAside: false,
+    isOpenModal: false,
+  };
+
+  handleToggleAside = () => {
+    const { isOpenAside } = this.state;
+    this.setState({ isOpenAside: !isOpenAside });
+  };
+
+  handleToggleModal = () => {
+    const { isOpenModal } = this.state;
+    this.setState({ isOpenModal: !isOpenModal });
+  };
 
   render() {
     return (
       <div>
-        <Button />
+        <div className={cx('box')}>
+          <Button onClick={this.handleToggleAside} type="button" text={this.isOpenAside ? 'Close Aside' : 'Open Aside'} />
+          <Button onClick={this.handleToggleModal} type="button" text={this.isOpenAside ? 'Close Modal' : 'Open Modal'} />
+        </div>
       </div>
     );
   }
