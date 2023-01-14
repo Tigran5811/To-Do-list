72bd8a28db259adf592fb39aa4f3016cd4ba21d9
Tigran5811 <tigran.bayramyan3@gmail.com>
2023-01-14 20:31:22 +0400

    main brench

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import { Button } from './components/button/Button';
 
 export class App extends Component {
-  constructor() {
-    super();
-    state = {
-      type: 'button',
-    };
-  }
-
   render() {
     return (
       <div>
