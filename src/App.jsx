c0ea652e99b73d257be8feca44eefed006a2cdba
Tigran5811 <tigran.bayramyan3@gmail.com>
2023-01-14 20:40:38 +0400

    my-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Button } from './components/button/Button';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Button />
-      </div>
+      <div />
     );
   }
 }
